9824d00af5171acb5d7be5534a2ec9e66247887f
m.buczek <m.buczek@samsung.com>
2023-09-12 21:42:52 +0200

    Put admin credentials to application.yaml

--- equipment-base-management-app/backend/src/main/java/org/buczek/engineering/thesis/app/equipmentbasemanagementapp/config/InitConfig.java
+++ equipment-base-management-app/backend/src/main/java/org/buczek/engineering/thesis/app/equipmentbasemanagementapp/config/InitConfig.java
@@ -1,26 +1,36 @@
 package org.buczek.engineering.thesis.app.equipmentbasemanagementapp.config;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.buczek.engineering.thesis.app.equipmentbasemanagementapp.security.UserRepository;
 import org.buczek.engineering.thesis.app.equipmentbasemanagementapp.security.model.Role;
 import org.buczek.engineering.thesis.app.equipmentbasemanagementapp.security.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
+@Slf4j
 public class InitConfig implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+
+    @Value("${application.credentials.admin1.email}")
+    private String admin1Email;
+
+    @Value("${application.credentials.admin1.password}")
+    private String admin1Password;
+
     @Override
     public void run(String... args) throws Exception {
         User admin = User.builder()
-                .email("marley@gmail.com")
+                .email(admin1Email)
                 .firstname("Bob")
                 .lastname("Marley")
-                .password(passwordEncoder.encode("password"))
+                .password(passwordEncoder.encode(admin1Password))
                 .role(Role.ADMIN)
                 .build();
 
